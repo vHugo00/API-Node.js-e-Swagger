2e0b24c36580bcfd0b3d80f56a35489e8a14606d
Vitor Hugo <vv001226@gmail.com>
2024-12-19 21:19:56 -0300

    Criando types

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
-import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+import type { FastifyInstance, RawServerDefault } from 'fastify'
+
+export type FastifyTypedInstance = FastifyInstance<
+  RawServerDefault
+// RowRequestDefaultExpressions
+>
